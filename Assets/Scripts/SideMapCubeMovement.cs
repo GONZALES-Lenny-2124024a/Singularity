201804ab8cd7efce60b0e29b0ad5e12a98f9ff15
Gonzales <lenny.gonzales@etu.univ-amu.fr>
2022-05-04 21:38:31 +0200

    SideMapCube's animation to up down cubes

--- Assets/Scripts/SideMapCubeMovement.cs
+++ Assets/Scripts/SideMapCubeMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SideMapCubeMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float beginTime;
+    private Animation animation;
+
     void Start()
     {
-        
+        beginTime = BeginTime();
+        Debug.Log(beginTime);
+        animation = GetComponent<Animation>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if(!animation.isPlaying) {
+            if (Time.realtimeSinceStartup > beginTime) {
+                animation.Play();
+            }
+        }
     }
+
+    public float BeginTime() {
+        return Random.Range(0f,10f);
+    }
+    
 }
